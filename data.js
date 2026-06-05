f9db6feb816877047112c5aefbea642823d77eaf
jasper6644112-afk <jasper6644112@gmail.com>
2026-06-05 23:40:20 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,32 @@
-m
+const BUSINESS_DATA = {
+
+lemonade: {
+name: "🍋 Lemonade Stand",
+cost: 500,
+income: 5
+},
+
+store: {
+name: "🏪 Convenience Store",
+cost: 5000,
+income: 50
+},
+
+restaurant: {
+name: "🍔 Restaurant",
+cost: 25000,
+income: 250
+}
+
+};
+
+let game = {
+money: 1000,
+incomePerSec: 0,
+
+businesses: {
+lemonade: 0,
+store: 0,
+restaurant: 0
+}
+};
