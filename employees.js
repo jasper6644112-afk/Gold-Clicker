739eb88434af6b97bd209d18f5886317bb567cf5
jasper6644112-afk <jasper6644112@gmail.com>
2026-06-07 11:39:45 +0500

    Update employees.js

--- employees.js
+++ employees.js
@@ -1,1 +1,53 @@
-.k
+const EMPLOYEE_DATA = {
+
+cashier:{
+name:"Cashier",
+cost:1000,
+incomeBoost:2
+},
+
+manager:{
+name:"Manager",
+cost:10000,
+incomeBoost:20
+},
+
+director:{
+name:"Director",
+cost:100000,
+incomeBoost:200
+}
+
+};
+
+game.employees = game.employees || {
+cashier:0,
+manager:0,
+director:0
+};
+
+function hireEmployee(type){
+
+const employee =
+EMPLOYEE_DATA[type];
+
+if(game.cash < employee.cost){
+
+alert("Not enough money!");
+
+return;
+
+}
+
+game.cash -= employee.cost;
+
+game.employees[type]++;
+
+game.incomePerSecond +=
+employee.incomeBoost;
+
+updateDashboard();
+
+saveGame();
+
+}
