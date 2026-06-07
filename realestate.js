96db62d9170b19d60372e75feef4d743f4357cd2
jasper6644112-afk <jasper6644112@gmail.com>
2026-06-07 11:41:54 +0500

    Update realestate.js

--- realestate.js
+++ realestate.js
@@ -1,1 +1,60 @@
-m
+const PROPERTY_DATA = {
+
+house:{
+name:"House",
+cost:50000,
+income:500
+},
+
+apartment:{
+name:"Apartment",
+cost:250000,
+income:2500
+},
+
+mansion:{
+name:"Mansion",
+cost:1000000,
+income:10000
+},
+
+office:{
+name:"Office Building",
+cost:5000000,
+income:50000
+}
+
+};
+
+game.realEstate = game.realEstate || {
+house:0,
+apartment:0,
+mansion:0,
+office:0
+};
+
+function buyProperty(type){
+
+const property =
+PROPERTY_DATA[type];
+
+if(game.cash < property.cost){
+
+alert("Not enough money!");
+
+return;
+
+}
+
+game.cash -= property.cost;
+
+game.realEstate[type]++;
+
+game.incomePerSecond +=
+property.income;
+
+updateDashboard();
+
+saveGame();
+
+}
