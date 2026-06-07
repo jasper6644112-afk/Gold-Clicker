0572096e1a685b8576716c1a5bc78fdf0d84f8b1
jasper6644112-afk <jasper6644112@gmail.com>
2026-06-07 11:38:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,89 @@
-function saveGame(){
+loadGame();
 
-localStorage.setItem(
-"businessEmpireSave",
-JSON.stringify(game)
-);
+function updateDashboard(){
+
+document.getElementById("cash")
+.textContent =
+"$" +
+Math.floor(game.cash)
+.toLocaleString();
+
+document.getElementById("netWorth")
+.textContent =
+"$" +
+Math.floor(game.netWorth)
+.toLocaleString();
+
+document.getElementById("income")
+.textContent =
+"$" +
+Math.floor(game.incomePerSecond)
+.toLocaleString();
+
+let businessCount = 0;
+
+for(const key in game.businesses){
+
+businessCount +=
+game.businesses[key];
 
 }
 
-function loadGame(){
+document.getElementById(
+"businessCount"
+).textContent =
+businessCount;
 
-const save =
-localStorage.getItem(
-"businessEmpireSave"
-);
+document.getElementById(
+"employeeCount"
+).textContent =
+Object.keys(
+game.employees
+).length;
 
-if(save){
+document.getElementById(
+"propertyCount"
+).textContent =
+Object.keys(
+game.realEstate
+).length;
 
-const data =
-JSON.parse(save);
-
-game = data;
+document.getElementById(
+"achievementCount"
+).textContent =
+game.achievements.length;
 
 }
 
+function calculateNetWorth(){
+
+let total = game.cash;
+
+for(const key in game.businesses){
+
+total +=
+game.businesses[key] *
+BUSINESS_DATA[key].cost;
+
 }
 
-setInterval(saveGame,5000);
+game.netWorth = total;
+
+}
+
+setInterval(() => {
+
+game.cash +=
+game.incomePerSecond;
+
+calculateNetWorth();
+
+updateDashboard();
+
+},1000);
+
+recalculateIncome();
+
+calculateNetWorth();
+
+updateDashboard();
