90ddd60f55a304a3f85c5fa2fb9193423d142a1a
jasper6644112-afk <jasper6644112@gmail.com>
2026-06-07 11:46:57 +0500

    Update bank.js

--- bank.js
+++ bank.js
@@ -1,1 +1,64 @@
-.
+game.bankBalance = game.bankBalance || 0;
+game.loanDebt = game.loanDebt || 0;
+
+function depositMoney(amount){
+
+if(game.cash < amount){
+
+alert("Not enough cash!");
+
+return;
+
+}
+
+game.cash -= amount;
+
+game.bankBalance += amount;
+
+updateDashboard();
+
+saveGame();
+
+}
+
+function withdrawMoney(amount){
+
+if(game.bankBalance < amount){
+
+alert("Not enough bank balance!");
+
+return;
+
+}
+
+game.bankBalance -= amount;
+
+game.cash += amount;
+
+updateDashboard();
+
+saveGame();
+
+}
+
+function takeLoan(amount){
+
+game.cash += amount;
+
+game.loanDebt += amount;
+
+updateDashboard();
+
+saveGame();
+
+}
+
+setInterval(() => {
+
+if(game.loanDebt > 0){
+
+game.loanDebt *= 1.001;
+
+}
+
+},10000);
